a9f7a826409847da6d9d13462ee015751bbfef4f
구기현 <rnrl4611@gmail.com>
2023-05-22 10:32:58 +0900

    feat: entity update

--- be/src/main/java/ssafy/antalbum/entity/like/LikeDate.java
+++ be/src/main/java/ssafy/antalbum/entity/like/LikeDate.java
@@ -1,9 +1,11 @@
 package ssafy.antalbum.entity.like;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 
 @Entity
+@DiscriminatorValue("D")
 @Getter
 public class LikeDate extends Like {
 
--- be/src/main/java/ssafy/antalbum/entity/like/LikeLocation.java
+++ be/src/main/java/ssafy/antalbum/entity/like/LikeLocation.java
@@ -1,10 +1,12 @@
 package ssafy.antalbum.entity.like;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 
 @Entity
+@DiscriminatorValue("L")
 @Getter
-public class LikeLocation extends Like {
+public class LikeLocation extends Like{
 
 }
